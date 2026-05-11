a297aeb965bf5702cd9fa387913f44ecc61ef0a0
Shivam Bhandari <168291459+shivtf@users.noreply.github.com>
2026-05-11 17:09:22 +0530

    testing

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 5000
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send("Server is running")
 })
 
 
 app.listen(port, () => {
-  console.log(`Hello from server at port ${port}`)
+  console.log(`Hello from server at port ${port} Shivam's laptop`)
 })
